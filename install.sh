44a078077e142791fa58beb79f971163ab7ae0e0
Grief <iamgrief@gmail.com>
2024-06-19 10:31:08 +0100

    plasma 6.1

--- install.sh
+++ install.sh
@@ -5,36 +5,43 @@ base="$(dirname "${BASH_SOURCE[0]}")"
 dir="${base}/osd"
 bottomPatch="${base}/bottom.patch"
 
-target='/usr/share/plasma/look-and-feel/org.kde.breeze.desktop/contents/osd'
-backup="${target}.orig"
+target1='/usr/share/plasma/look-and-feel/org.kde.breeze.desktop/contents/osd'
+target2='/usr/share/plasma/shells/org.kde.plasma.desktop/contents/osd'
+backup1="${target1}.orig"
+backup2="${target2}.orig"
 
 case "$1" in
   'install')
-    if [ -d "${backup}" ]; then
+    if [ -d "${backup1}" ] || [ -d "${backup2}" ]; then
        echo "breeze-os-top is already installed (use 'restore' to rollback or 'forget' to remove the backup)"
        exit 1
     fi
-    sudo mv "${target}" "${backup}"
-    sudo cp -ra "${dir}" "${target}"
+    sudo mv "${target1}" "${backup1}"
+    sudo cp -ra "${dir}" "${target1}"
+    sudo mv "${target2}" "${backup2}"
+    sudo cp -ra "${dir}" "${target2}"
     if [ "$2" == 'bottom' ]; then
-      patch -d "${target}" < "${bottomPatch}"
+      patch -d "${target1}" < "${bottomPatch}"
+      patch -d "${target2}" < "${bottomPatch}"
     fi
-    sudo chown -R root:root "${target}"
+    sudo chown -R root:root "${target1}" "${target2}"
     ;;
   'restore')
-    if [ ! -d "${backup}" ]; then
+    if [ ! -d "${backup1}" ] || [ ! -d "${backup2}" ]; then
        echo "breeze-os-top is not installed"
        exit 1
     fi
-    sudo rm -rf "${target}"
-    sudo mv "${backup}" "${target}"
+    sudo rm -rf "${target1}"
+    sudo mv "${backup1}" "${target1}"
+    sudo rm -rf "${target2}"
+    sudo mv "${backup2}" "${target2}"
     ;;
   'forget')
-      if [ ! -d "${backup}" ]; then
+      if [ ! -d "${backup1}" ] && [ ! -d "${backup2}" ]; then
          echo "breeze-os-top is not installed"
          exit 1
       fi
-      sudo rm -rf "${backup}"
+      sudo rm -rf "${backup1}" "${backup2}"
       exit 0
       ;;
   *)
